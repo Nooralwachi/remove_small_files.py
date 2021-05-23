7fef6a53aca8246b4ff2803919588c6d6fed3c84
Nooralwachi <noor_alwachi@yahoo.com>
2021-05-22 21:03:10 -0700

    Update remove_small_files.py

--- remove_small_files.py
+++ remove_small_files.py
@@ -5,7 +5,7 @@ def remove_small_files(dir_name):
 	for file in os.listdir(dir_name):
 		size= os.path.getsize(dir_name +'/'+ file)
 		files.append([file,size])
-		sorted_files =sorted(files, key = lambda x:x[0])
+	sorted_files =sorted(files, key = lambda x:x[0])
 	for file in sorted_files:
 		print(f'{file[0]} {file[1]/1000:.1f}KB')
 	for file in sorted_files:
